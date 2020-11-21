1236af9012e32b9c25cf61f12bedb34b4e6f54c3
Chris <fitzer.c@gmail.com>
2020-11-21 12:02:03 -0600

    Initial project setup

---
+++ ShoppingListUno/ShoppingListUno.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace ShoppingListUno.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new ShoppingListUno.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ ShoppingListUno/ShoppingListUno.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace ShoppingListUno.Skia.Gtk
+{
+}
